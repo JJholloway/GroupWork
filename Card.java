82dc7f1a2f7bcc0b7579ee79d45e5ac13ee38437
Tiago Dall Igna <tiagodll@gmail.com>
2013-02-28 21:22:20 +0100

    Cards class

---
+++ Card.java
@@ -0,0 +1,34 @@
+public class Card {
+	private int number;
+	private suits suit;
+	public enum suits{ Clubs, Diamonds, Hearts, Spades }
+
+	public Card(int n, suits s){
+		number = n;
+		suit = s;
+	}
+
+	public String toString(){ 
+		String numpart;
+
+		switch(number){
+			case 0: numpart = "Ace"; break;
+			case 11: numpart = "Jack"; break;
+			case 12: numpart = "Queen"; break;
+			case 13: numpart = "King"; break;
+			default: numpart = number + "";
+		}
+		return numpart + " of " + suit;
+	}
+
+	/*	If you like to gamble, I tell you I'm your man
+		You win some, lose some, it's - all - the same to me
+		The pleasure is to play, it makes no difference what you say
+		I don't share your greed, the only card I need is
+		The Ace Of Spades
+		The Ace Of Spades	*/
+	public static void main(String[] args){
+		Card c = new Card(0, suits.Spades);
+		System.out.println(c);
+	}
+}
